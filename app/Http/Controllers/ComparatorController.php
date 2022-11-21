fbb571fb34daff9629e166ac762d3f4e0a3343b7
grgic.matej <grgic.matej@gmail.com>
2022-11-21 13:55:15 +0100

    feat: API endpoint
    Added API endpoint for calculating comparison result.

--- app/Http/Controllers/ComparatorController.php
+++ app/Http/Controllers/ComparatorController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ComparatorController extends Controller
@@ -22,6 +23,29 @@ class ComparatorController extends Controller
         return view('result', [
             'result' => $this->compare($request)
         ]);
+    }
+
+    /**
+     * Get ratio result and return as API endpoint
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getResultApi(Request $request): JsonResponse
+    {
+        if (!isset($_GET['words_stream_1']) || !isset($_GET['words_stream_2'])) {
+            return response()->json('missing/wrong parameters', 400);
+        }
+
+        if ($request->getMethod() !== 'GET') {
+            return response()->json('wrong method, only GET allowed', 405);
+        }
+
+        if ($request->header('key') === null) {
+            return response()->json('wrong authorization, check how to', 401);
+        }
+
+        return response()->json($this->compare($request));
     }
 
     /**
