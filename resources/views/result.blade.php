5cfb3eb64938ac26f42599b98159f1ab752e69ec
grgic.matej <grgic.matej@gmail.com>
2022-11-21 12:40:48 +0100

    feat: Another comparison & result page
    Enabled another comparison, optimization of result page.

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -15,12 +15,12 @@
 <body class="litipsum text pride-and-prejudice">
 <header class="header">
     <h1 class="page-title"><a href="/" class="page-title__link">Similaritipsum</a></h1>
-    <menu id="nav-menu" class="nav-menu">
+    <div id="nav-menu" class="nav-menu">
         <a href="{{ url('how') }}" id="nav-menu-how-to" data-target="help-how-to" class="nav-menu__link nav-menu__help-how-to">How to
             use</a>
         <a href="{{ url('api') }}" id="nav-menu-api" data-target="help-api" class="nav-menu__link nav-menu__help-api">API</a>
         <a href="https://github.com/grgicmatej" id="nav-menu-api" data-target="help-api" class="nav-menu__link nav-menu__help-api">Grgic Matej</a>
-    </menu>
+    </div>
 </header>
 
 
@@ -35,21 +35,23 @@
         @endif
     </div>
 
-    <menu id="text-buttons" class="text-buttons buttons">
-        <button type="submit" form="streaminput" class="text-buttons__button button pride-and-prejudice selected"
-                value="pride-and-prejudice">Check another similarity
-        </button>
-    </menu>
+    <div id="text-buttons" class="text-buttons buttons">
+        <input type="submit" form="streaminput" class="text-buttons__button button pride-and-prejudice selected"
+                value="Check another similarity">
+
+    </div>
 
     <div id="preview" class="preview">
-        <form action="/getResult" id="streaminput" method="post">
+        <form action="/get-result" id="streaminput" method="post">
             @csrf
             <p>
-                <textarea class="words_stream" name="words_stream_1" id="" rows="10"
+                <label for="words_stream_1"></label>
+                <textarea class="words_stream" name="words_stream_1" id="words_stream_1" rows="10"
                           placeholder="Insert first set of words here" required></textarea>
             </p>
             <p>
-                <textarea class="words_stream" name="words_stream_2" id="" rows="10"
+                <label for="words_stream_2"></label>
+                <textarea class="words_stream" name="words_stream_2" id="words_stream_2" rows="10"
                           placeholder="Insert second set of words here" required></textarea></p>
         </form>
 
